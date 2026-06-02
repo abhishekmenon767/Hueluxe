b95195c5b4d093a212c269135ef892cad437f544
abhishekiocod <abhishek@iocod.com>
2026-06-02 15:02:12 +0530

    feat: update preview teaser logo to isolated version

--- src/components/PreviewTeaser.js
+++ src/components/PreviewTeaser.js
@@ -14,7 +14,7 @@ export default function PreviewTeaser() {
           transition={{ duration: 1 }}
           style={{ height: "550px", position: "relative", overflow: "hidden", display: "flex", alignItems: "center", justifyContent: "center" }}
         >
-          <img src="/logonew.png" alt="Hueluxe Official Logo" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+          <img src="/logonew_isolated.png" alt="Hueluxe Official Logo" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
         </motion.div>
 
         <motion.div initial={{ opacity: 0, x: 30 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, margin: "-100px" }} transition={{ duration: 0.8 }} style={{ padding: "20px 0" }}>
